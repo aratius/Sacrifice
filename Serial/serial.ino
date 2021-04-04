174a9e0584b14bf74bc6185d8578faea1e9723d2
aratius <arata1129matsu@icloud.com>
2021-04-04 20:57:34 +0900

    serial送信

--- Serial/serial.ino
+++ Serial/serial.ino
@@ -1,14 +1,18 @@
 const int TOP = 4;
 bool top = false;
+String topName = "top";
 
 const int BOTTOM = 2;
 bool bottom;
+String bottomName = "bottom";
 
 const int LEFT = 5;
 bool left = false; 
+String leftName = "left";
 
 const int RIGHT = 3;
 bool right = false;
+String rightName = "right";
 
 
 void setup() {
@@ -17,30 +21,36 @@ void setup() {
 }
 
 void loop() {
-  bool _left = checkButton(LEFT, left);
+  bool _left = checkButton(LEFT, left, leftName);
   if(_left) left = !left;
 
-  bool _top = checkButton(TOP, top);
+  bool _top = checkButton(TOP, top, topName);
   if(_top) top = !top;
 
-  bool _right = checkButton(RIGHT, right);
+  bool _right = checkButton(RIGHT, right, rightName);
   if(_right) right = !right;
 
-  bool _bottom = checkButton(BOTTOM, bottom);
+  bool _bottom = checkButton(BOTTOM, bottom, bottomName);
   if(_bottom) bottom = !bottom;
   
   delay(10);
 }
 
-bool checkButton(int pinNumber, bool isShining){
+bool checkButton(int pinNumber, bool isShining, String key){
   if(digitalRead(pinNumber) == HIGH && !isShining) {
-    Serial.println("button state became high");
-    Serial.print("\t");
+    SendSerial(key, 1);
     return true;
   }else if(digitalRead(pinNumber) == LOW && isShining) {
-    Serial.println("button state became low");
-    Serial.print("\t");
+    SendSerial(key, 0);
     return true;
   }
   return false;
+}
+
+void SendSerial(String key, int val) {
+  Serial.print(key);
+  Serial.print("\t");
+  Serial.print(val);
+  Serial.print("\t");
+  Serial.println();
 }
