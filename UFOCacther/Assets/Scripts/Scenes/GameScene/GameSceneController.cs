301da7e0781ff91db7fde1e86eec244e95862c1a
aratius <arata1129matsu@icloud.com>
2021-04-24 19:33:05 +0900

    自動生成されたファイル

---
+++ UFOCacther/Assets/Scripts/Scenes/GameScene/GameSceneController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneController : SceneControllerBase
+{
+    public override void _Start()
+    {
+
+    }
+
+    public override void _Update()
+    {
+
+    }
+}
