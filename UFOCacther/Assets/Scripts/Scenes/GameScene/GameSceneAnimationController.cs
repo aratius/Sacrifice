3e1252873dcf13bfca7e063779371125fb4a9c00
aratius <arata1129matsu@icloud.com>
2021-04-24 19:37:19 +0900

    animationControllerをそれぞれ追加

---
+++ UFOCacther/Assets/Scripts/Scenes/GameScene/GameSceneAnimationController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneAnimationController : SceneControllerBase
+{
+    public override void _Start()
+    {
+
+    }
+
+    public override void _Update()
+    {
+
+    }
+}
